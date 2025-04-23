004949780a9e4e9920b9eef399d5efbd298278dd
Charles Franklin Jahn <charlesjahn@gmail.com>
2025-04-23 21:18:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Definir a data de término
-var countDownDate = new Date("Jan 30, 2025 20:00:00 GMT-0300").getTime();
+var countDownDate = new Date("Jun 24, 2025 14:10:00 GMT 0000").getTime();
 
 // Atualizar a contagem regressiva a cada 1 segundo
 var countdownFunction = setInterval(function() {
